ca75c3903d6aad4a8752b12d220f4d1f7a065d3d
DESKTOP-P4USMVD\Usuario <jhonlola1234@gmail.com>
2025-11-16 17:38:42 -0500

    Ejecicio 2

---
+++ Jhon-Angarita/Ejercicio2.js
@@ -0,0 +1,11 @@
+let tareas = ["Revisar emails"];
+
+tareas.unshift("Llamar al cliente");
+tareas.unshift("Preparar reporte");
+
+console.log(tareas);
+
+tareas.shift(); 
+tareas.shift(); 
+
+console.log(tareas);
