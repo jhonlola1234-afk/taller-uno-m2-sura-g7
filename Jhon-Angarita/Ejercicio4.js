2eabace54b48e46189052c931482adecb79d1ab0
DESKTOP-P4USMVD\Usuario <jhonlola1234@gmail.com>
2025-11-16 17:57:44 -0500

    Ejecicio 4

---
+++ Jhon-Angarita/Ejercicio4.js
@@ -0,0 +1,15 @@
+let colaImpresion = ["doc1.pdf", "doc2.png", "doc3.jpg"];
+
+console.log(colaImpresion);
+
+colaImpresion.shift();
+
+console.log(colaImpresion);
+
+colaImpresion.push("doc4.txt");
+
+console.log(colaImpresion);
+
+colaImpresion.shift();
+
+console.log(colaImpresion);
