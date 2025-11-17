bbfb6f3f6f0d15c3efda5c2813eb4738ca7b4b4a
DESKTOP-P4USMVD\Usuario <jhonlola1234@gmail.com>
2025-11-17 17:53:46 -0500

    Ejecicio 11

---
+++ Jhon-Angarita/Ejercicio11.js
@@ -0,0 +1,11 @@
+let carrito = [];
+
+carrito.push("Camisa","Pantalon");
+
+console.log(carrito);
+
+carrito.unshift("Zapatos")
+console.log(carrito);
+
+carrito.splice(1, 1); 
+console.log(carrito);
