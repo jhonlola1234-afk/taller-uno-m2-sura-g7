ef7aadb0781f1ce524adf0e4c64c0bc7924e55f0
DESKTOP-P4USMVD\Usuario <jhonlola1234@gmail.com>
2025-11-17 18:06:00 -0500

    Ejecicio 13

---
+++ Jhon-Angarita/Ejercicio13.js
@@ -0,0 +1,8 @@
+let receta = ["Paso 2: Mezclar ingredientes", "Paso 3: Hornear"];
+console.log(receta)
+
+receta.unshift("Paso 1: Pre-calentar horno");
+console.log(receta)
+
+receta.push("Paso 4: Servir");
+console.log(receta)
