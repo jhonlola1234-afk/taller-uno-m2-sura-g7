874c609ac92ce9d6997e373ecca8b1daa7abf13c
DESKTOP-P4USMVD\Usuario <jhonlola1234@gmail.com>
2025-11-17 18:22:39 -0500

    Ejecicio 15

---
+++ Jhon-Angarita/Ejercicio15.js
@@ -0,0 +1,8 @@
+let equipo = ["Ana (Líder)", "Juan (Dev)", "Maria (Diseñadora)"];
+console.log(equipo);
+
+equipo.splice(1, 1); 
+console.log(equipo);
+
+equipo.push("Pedro (Dev)", "Luisa (Dev)");
+console.log(equipo);
