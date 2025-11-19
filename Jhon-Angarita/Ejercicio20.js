d65184160b770273ded3471053571506dea4532f
DESKTOP-P4USMVD\Usuario <jhonlola1234@gmail.com>
2025-11-18 21:02:26 -0500

    Ejecicio 20

---
+++ Jhon-Angarita/Ejercicio20.js
@@ -0,0 +1,14 @@
+let codigos = ["A-10", "C-30", "B-20", "D-40"];
+console.log(codigos);
+
+codigos.sort();  
+console.log(codigos);
+
+codigos.splice(2, 1);    
+console.log(codigos);
+
+codigos.unshift("X-99"); 
+console.log(codigos);
+
+codigos.reverse();      
+console.log(codigos);  
